6e2d13141ef1a960ed8f52a3246a5a5019446c9b
thaismota-tech <thaissantosdamota@gmail.com>
2026-05-10 16:55:42 +0200

    He finalizado el ejercicio 5

--- index.js
+++ index.js
@@ -44,8 +44,9 @@ async function printImageAndName(pokemonNameOrId) {
         const pokemon = await getImageAndName(pokemonNameOrId);
         if (pokemon) {
             return `<section>
-    <img src="${pokemon.img}" alt="${pokemon.name}">
-    <h1>Nombre del pokemon:${pokemon.name}</h1></section>`;
+    <img src="${pokemon.img}" alt="nombre del pokemon${pokemon.name}">
+    <h1>Nombre del pokemon: ${pokemon.name}</h1>
+    </section>`;
         }
         return '';
     } catch (error) {
@@ -59,3 +60,27 @@ async function pintarPokemon() {
     }
 }
 pintarPokemon();
+
+// 4- Declara una función getRandomDogImage que retorne la url de la imagen de un perro aleatorio
+async function getRandomDogImage() {
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        return data.message;
+    } catch (error) {
+        console.error("Error al obtener el perro:", error);
+    }
+}
+
+//5 - Declara una función getRandomPokemonImage que retorne la url de la imagen de un pokemon aleatorio.
+async function getRandomPokemonImage() {
+    try {
+        const randomId = Math.floor(Math.random() * 1025) + 1;
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+        const data = await response.json();
+        return data.sprites.front_default;
+    } catch (error) {
+        console.error("Error al obtener la url del Pokémon aleatorio:", error);
+    }
+}
